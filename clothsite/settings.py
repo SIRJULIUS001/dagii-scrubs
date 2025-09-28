ac9d64539165e5832f42aebc5a6847593199db68
sirjulius <ntoitijulius01@gmail.com>
2025-09-28 03:58:15 +0300

    Prepare Django for Render deployment

--- clothsite/settings.py
+++ clothsite/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'store' /'static']
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
